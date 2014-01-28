9635c7e20e8596978fd73262e77d0b555c31a1d5
Todd Gureckis <todd.gureckis@nyu.edu>
2014-01-28 12:18:47 -0500

    upload script
    - requires config.py (not in git) to set hostname, etc..

---
+++ upload.py
@@ -0,0 +1,26 @@
+from paramiko import SSHClient
+from scp import SCPClient
+import os, sys
+from config import *
+
+manifest = ['index.html',
+			'syllabus.html',
+			'lecture1.html',
+			'build_site.py',
+			'LICENSE',
+			'README.md',		
+			'css',
+			'js',
+			'fonts']
+
+ssh = SSHClient()
+ssh.load_system_host_keys()
+ssh.connect(HOST,username=USERNAME,password=PASSWORD)
+
+# SCPCLient takes a paramiko transport as its only argument
+scp = SCPClient(ssh.get_transport())
+
+print manifest
+for item in manifest:
+	if item is not 'config.py':
+		scp.put(item, BASEPATH, recursive=True)
